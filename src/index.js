61572d01be94a7c55985b1131c991d3bb524908e
Eric Fuhrmann <Eric Fuhrmann>
2016-10-25 00:22:04 -0400

    Advanced to 'State vs Props in React' lesson

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('React is up and running!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Detail from './pages/Detail';
+
+ReactDOM.render (
+    <Detail message="This is coming from props!" />,
+    document.getElementById('app')
+);
+
+// Required for hot reload
+module.hot.accept();
